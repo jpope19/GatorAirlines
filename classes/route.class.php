f4e93e394e70bf4f7b63bc1541a3da467ba26e80
Cheran Wu <cheranwu@gmail.com>
2012-10-10 01:57:02 -0400

    search working and tested

--- classes/route.class.php
+++ classes/route.class.php
@@ -1,43 +1,52 @@
-<? php
+<?php
 class Route {
-  private var $flights = new Array();
-  private var $avail_tickets;
-  private var $layover_time;
-  private var $num_flights;
-  
-  function __construct() {
-    this->num_flights = 0;
-  }
-  
-  public function add_flight($flight) {
-    this->$flights[num_flights] = $flight;
-    this->num_flights++;
-    this->$layover_time += ($flight['e_depart_time']) - this->$flights[num_flights-2]['e_arrival_time'];
-  }
-  
-  public function get_trip_time() {
-    if(this->num_flights == 0) return 0;
-    return this->$flights[num_flights-1]['e_arrival_time'] - this->$flights[0]['e_depart_time']
-  }
-  
-  public function get_layover_time() {
-    return this->layover_time;
-  }
-  
-  public function get_flights() {
-    return this->$flights;
-  }
-  
-  public function get_dest() {
-    return this->$flights[num_flights-1]['dest_id'];
-  }
-  
-  public function copy() {
-    Route $copy = new Route();
-    foreach($flights as $flight) {
-      $copy.add($flight);
+    var $avail_tickets;
+    var $layover_time;
+    var $num_flights;
+    
+    function __construct() {
+        $this->num_flights = 0;
+        $ths->flights = array();
     }
-    return $copy;
-  }
+    
+    public function add_flight($flight) {
+        $this->flights[] = $flight;
+        $this->num_flights += 1;
+        if($this->num_flights >= 2) {
+            $this->layover_time += ($flight['e_depart_time']) - $this->flights[$this->num_flights-2]['e_arrival_time'];
+        }
+    }
+    
+    public function get_trip_time() {
+        if($this->num_flights == 0) return 0;
+        else {
+            //echo $this->flights[$this->num_flights-1]['e_arrival_time'] - $this->flights[0]['e_depart_time'];
+            return $this->flights[$this->num_flights-1]['e_arrival_time'] - $this->flights[0]['e_depart_time'];
+        }
+    }
+    
+    public function get_layover_time() {
+        return $this->layover_time;
+    }
+    
+    public function get_flights() {
+        return $this->flights;
+    }
+    
+    public function get_dest() {
+        return $this->flights[$this->num_flights-1]['dest_id'];
+    }
+    
+    public function get_arrival_time() {
+        return $this->flights[$this->num_flights-1]['e_arrival_time'];
+    }
+    
+    public function copy() {
+        $copy = new Route();
+        foreach($this->flights as $flight) {
+            $copy->add_flight($flight);
+        }
+        return $copy;
+    }
 }
 ?>
