66a8e7723c32276843cb1890980c6aea9ee04a24
Carlos Vasquez <cavasquez@ufl.edu>
2012-10-14 17:24:27 -0400

    Jquery validation for addcustomer is working

--- admin/AddCustomer.php
+++ admin/AddCustomer.php
@@ -97,8 +97,118 @@ if (isset($_POST['AddCustomerSubmit']))
 <!-- Jquery that uses Validation plugin to validate form on client side -->
 <script>
 $(document).ready(function()
-{
-    $("#AddCustomerForm").validate();
+{	// Rule for names
+	$.validator.addMethod("alphabet", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z]+$/i.test(value);
+    }, "Your alphabet may only contain letters.");
+	
+	$.validator.addMethod("address", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9 ]+$/i.test(value);
+    }, "The address may only contain letters and numbers.");
+	
+	$.validator.addMethod("zip", function(value, element) {
+        return this.optional(element) || /^[]+$/i.test(value);
+    }, "The address may only contain letters and numbers.");
+	
+    $("#AddCustomerForm").validate({
+		// Apply rules
+		
+		rules: {
+			"email":{
+				required: true,
+				alphabet: false,
+				address: false
+			},
+			"first_name": {
+				required: true,
+				alphabet: true,
+				address: false
+			},
+			"last_name": {
+				requred: true,
+				alphabet: false,
+				address: false
+			},
+			"password": {
+				required: true,
+				alphabet: false,
+				address: false
+			},
+			"addr": {
+				required: true,
+				alphabet: false,
+				address: true
+			},
+			"city": {
+				required: true,
+				alphabet: true,
+				address: false
+			},
+			"state": {
+				required: true,
+				alphabet: true,
+				address: false
+			},
+			"zip": {
+				required: true,
+				alphabet: false,
+				address: false,
+				digits: true,
+				rangelength: [5,5]
+			},
+			"cc_num": {
+				required: true,
+				alphabet: false,
+				address: false
+			},
+			"u_type": {
+				required: true,
+				alphabet: false,
+				address: false,
+				digits: true,
+				rangelength: [1,1]
+			}
+		},
+		messages: {
+			"email": {
+				required: "You must provide an email.",
+			},
+			"first_name": {
+				required: "You must provide a first name.",
+				alphabet: "Your first name may only contain letters."
+			},
+			"last_name": {
+				required: "You must provide a last name.",
+				alphabet: "Your last name may only contian letters."
+			},
+			"password": {
+				required: "You must provide a password.",
+			},
+			"addr": {
+				required: "You must provide the street of the billing address.",
+			},
+			"city": {
+				required: "You must provide the city of the billing address.",
+				alphabet: "The city may only be letters."
+			},
+			"state": {
+				required: "You must provide the state of the billing address.",
+				alphabet: "The state may only be letters."
+			},
+			"zip": {
+				required: "You must provide the zip of the billing addrss.",
+				rangelength: "Your zip code must be 5 digits long." 
+			},
+			"cc_num": {
+				required: "You must provide your credit card number.",
+			},
+			"u_type": {
+				required: "You must provide a user type.",
+				digits: "The user type can only be a number",
+				rangelength: "The user type is only 1 digit."
+			}
+		}
+	});
 });
 </script>
 
@@ -112,7 +222,7 @@ Billing Address: <input type="text" class="required" name="addr" /> </br>
 City: <input type="text" class="required" name="city" /> </br>
 State: <input type="text" class="required" name="state" /> </br>
 Zip Code: <input type="text" class="required" name="zip" /> </br>
-Credit Card Number: <input type="text" class="required" name="cc_num" /> </br>
+Credit Card Number: <input type="text" class="required creditcard" name="cc_num" /> </br>
 User Type: <input type="text" class="required" name="u_type" /> </br>
 <input class="submit" type="submit" name="AddCustomerSubmit"/>
 </form>
