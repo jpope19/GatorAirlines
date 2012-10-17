15a9749334064c7be4d586c40976555965e1c434
Carlos Vasquez <cavasquez@ufl.edu>
2012-10-16 21:13:21 -0400

    Some modifications to modify customer (cleaned up checks)

--- admin/ModifyCustomer.php
+++ admin/ModifyCustomer.php
@@ -292,16 +292,3 @@ if (isset($_POST['ModifyCustomerSubmit']))
 </tr>
 </br> <input type="submit" name="ModifyCustomerSubmit" /> 
 </form>
-
-<script type="text/javascript">
-	document.getElementById("email").value="asd@asd.com";
-	document.getElementById("first_name").value="asd";
-	document.getElementById("last_name").value="asd";
-	document.getElementById("password").value="asdasdasdasd";
-	document.getElementById("addr").value="165a asd ";
-	document.getElementById("city").value="Asd";
-	document.getElementById("state").value="Florida";
-	document.getElementById("zip").value="32608";
-	document.getElementById("cc_num").value="1111111155555555";
-	document.getElementById("u_type").value="0";
-</script>
