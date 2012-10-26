8ca412078b3508e7ab48256afc598c1a0d00300d
Marysol Ortiz <maddiesolz@gmail.com>
2012-10-26 00:07:35 -0400

    Reward Page

---
+++ GatorAirlines2/site/reward_chart.php
@@ -0,0 +1,86 @@
+<?php
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Reward Chart</title>
+
+
+</head>
+
+
+<body id="page4">
+
+
+<div class="main">
+<!--header -->
+
+<!-- / header -->
+
+
+<!--content -->
+	<section id="content">
+		   <div class="wrapper pad1">
+		   
+		<table border="1" style="background-color:white;border:1px dotted black;width:80%;border-collapse:collapse;">
+<tr style="background-color:orange;color:white;">
+<th style="padding:3px;">To/From</th><th style="padding:10px;">To/From</th>
+<th style="padding:3px;">Economy</th><th style="padding:3px;">Business</th>
+<th style="padding:3px;">Private</th>
+</tr>
+<tr>
+<td style="padding:15px;">US/Canada</td><td style="padding:3px;">US/Canada</td><td style="padding:3px;">12,500-25,000 miles</td> <!--ROWS-->
+<td style="padding:10px;">25,000-50,000 miles</td><td style="padding:3px;">32,000-65,000 miles</td>
+</tr>
+<tr>
+<td style="padding:15px;">US/Canada/Mex</td><td style="padding:3px;">US/Canada/Mex</td><td style="padding:3px;">15,500-30,000 miles</td> <!--ROWS-->
+<td style="padding:10px;">28,000-58,000 miles</td><td style="padding:3px;">40,000-75,000 miles</td>
+</tr>
+<tr>
+<td style="padding:10px;">US/Canada/Mex</td><td style="padding:3px;">US/Canada/Mex</td><td style="padding:3px;">15,500-30,000 miles</td> <!--ROWS-->
+<td style="padding:10px;">28,000-58,000 miles</td><td style="padding:3px;">40,000-75,000 miles</td>
+</tr>
+<tr>
+<td style="padding:15px;">US/Canada/Mex</td><td style="padding:3px;">US/Canada/Mex</td><td style="padding:3px;">15,500-30,000 miles</td> <!--ROWS-->
+<td style="padding:10px;">28,000-58,000 miles</td><td style="padding:3px;">40,000-75,000 miles</td>
+</tr>
+<tr>
+<td style="padding:15px;">US/Canada/Mex</td><td style="padding:3px;">US/Canada/Mex</td><td style="padding:3px;">15,500-30,000 miles</td> <!--ROWS-->
+<td style="padding:10px;">28,000-58,000 miles</td><td style="padding:3px;">40,000-75,000 miles</td>
+</tr>
+</table>
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+	  
+				        </div>
+			</section>
+			
+			
+			
+			
+			
+			
+			
+			
+
+		</div>
+
+</body>
+</html>
