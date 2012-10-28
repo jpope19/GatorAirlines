f16bd9a174d41bf8529428f8ed765492037f23a6
Jeff Pope <jpope@mitchell-networks.com>
2012-10-28 15:23:17 -0400

    Got rid of old stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-GatorAirlines
-=============
-
-UF Software Engineering Project
--- SQL/droptables.sql
+++
@@ -1,6 +0,0 @@
-DROP table customers;       
-DROP table airports;
-DROP table airplanes;      
-DROP table flights;
-DROP table tickets;
-DROP table vip;
