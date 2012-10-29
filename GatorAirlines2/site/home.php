64f59509447465b20cd3b4fe1cb976f288dac23b
Carlos Vasquez <cavasquez@ufl.edu>
2012-10-28 20:29:06 -0400

    Rmove a non exitent link

--- GatorAirlines2/site/home.php
+++ GatorAirlines2/site/home.php
@@ -32,7 +32,7 @@ $airports = $users->get_airports();
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
     <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
     <script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
-    <link rel="stylesheet" href="/resources/demos/style.css" />
+
 
 	<script>
     $(function() {
